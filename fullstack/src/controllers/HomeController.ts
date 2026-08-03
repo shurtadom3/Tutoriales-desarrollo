aa456e26a0762648ed55cf988ef6cb7373c7cfd9
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-08-02 19:16:44 -0500

    feat: implement show action in HomeController

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express'; 
 import { books } from '../data/books.js';
+import { Book } from '../models/Book.js'; 
 
 export class HomeController { 
     static index(req: Request, res: Response): void { 
@@ -13,16 +14,22 @@ export class HomeController {
         viewData["title"] = "About"; 
         res.render('home/about', { viewData: viewData }); 
   } 
+
     static contact(req: Request, res: Response): void {
         const viewData: { [key: string]: any } = {};
         viewData["title"] = "Contact";
         res.render('home/contact', { viewData: viewData });
     }
+
     static Main_Point(req: Request, res: any) { 
     const viewData: any = {}; 
      viewData["books"] = books; 
     res.render('home/books', viewData); 
+  }
 
+     static show(req: any, res: any) { 
+    const book = Book.findById(books, parseInt(req.params.id)); 
+    res.render('home/show', { book: book }) 
   } 
 } 
 
