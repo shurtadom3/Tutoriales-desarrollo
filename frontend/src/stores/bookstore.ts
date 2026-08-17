50114178c0317b74659bfffb9034da5648c161cd
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-08-17 18:20:23 -0500

    feat: add book store and seeder

---
+++ frontend/src/stores/bookstore.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'; 
+import { ref } from 'vue'; 
+import type { BookInterface } from '@/interfaces/BookInterface.js'; 
+
+export const useBookStore = defineStore('book', () => { 
+  const books = ref<BookInterface[]>([]); 
+  return { books, }; 
+}); 
