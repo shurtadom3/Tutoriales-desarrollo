576c3735d3e28924dad788e36a2540959d6b2278
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-08-17 18:42:53 -0500

    feat: add book creation route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,13 +3,14 @@ import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue'; 
 import BooksIndexView from '@/views/BooksIndexView.vue'; 
 import BooksShowView from '@/views/BooksShowView.vue'; 
-
+import BooksCreateView from '@/views/BooksCreateView.vue'; 
 
 const router = createRouter({ 
   history: createWebHistory(import.meta.env.BASE_URL), 
   routes: [ 
 
     { path: '/', name: 'home', component: HomeView, meta: { title: 'Home' } }, 
+    { path: '/books/create', name: 'books.create', component: BooksCreateView, meta: { title: 'Create Book' } },
     { path: '/about', name: 'about', component: AboutView, meta: { title: 'About' } }, 
     { path: '/books', name: 'books', component: BooksIndexView, meta: { title: 'Books' } }, 
     { path: '/books/:id', name: 'book', component: BooksShowView, meta: { title: 'Book' } }, 
