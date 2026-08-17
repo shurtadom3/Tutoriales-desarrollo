450d62326abff9aeb2c73bdda5e5a6410be42c50
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-08-17 18:42:27 -0500

    feat: add book creation service

--- frontend/src/services/BookService.ts
+++ frontend/src/services/BookService.ts
@@ -1,5 +1,6 @@
 import { useBookStore } from '@/stores/bookstore.js'; 
 import type { BookInterface } from '@/interfaces/BookInterface'; 
+import type { CreateBookDTO } from '@/dtos/CreateBookDTO.js'; 
 
 export class BookService { 
   static getBooks(): BookInterface[] { 
@@ -7,5 +8,10 @@ export class BookService {
 
   static getBookById(id: number): BookInterface | undefined { 
     return useBookStore().books.find((book) => book.id === id); 
+  } 
+
+  static createBook(book: CreateBookDTO): void { 
+    const id = useBookStore().books.length + 1; 
+    useBookStore().books.push({ id, ...book }); 
   } 
 } 
