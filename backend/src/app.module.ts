ef67299846903018ec02e722d8d8464deac1a215
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-09-12 19:32:26 -0500

    feat: add home module and home controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import { Module } from '@nestjs/common'; 
+import { HomeModule } from './home/home.module.js'; 
+ 
+@Module({ 
+  imports: [HomeModule], 
+}) 
+export class AppModule {} 
