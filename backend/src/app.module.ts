53e23b28dae57ec38dd4cbbad2f9363fbba7fe4a
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-09-12 19:41:18 -0500

    feat: configure TypeORM with SQLite database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'; 
 import { HomeModule } from './home/home.module.js'; 
- 
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+
 @Module({ 
-  imports: [HomeModule], 
+  imports: [
+      TypeOrmModule.forRoot({ 
+       type: 'better-sqlite3', 
+       database: 'database.sqlite', 
+       autoLoadEntities: true, 
+       synchronize: true, 
+    }), 
+    HomeModule], 
 }) 
 export class AppModule {} 
